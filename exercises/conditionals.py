1235177ca1b5505e7a280330472bcfba1f1957c5
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 18:29:54 +0300

    sdfghjk

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,24 @@
-# Write your solution for 1.2 here!
+# sum=0
+# i=0
+# while sum<=10000 :
+# 	if(i%2==0):
+# 		sum=sum+i
+# 	i+=1
+
+# print(i-1)
+
+def is_prime(x):
+	i=2
+	while i<x:
+		if(x%i==0):
+			return "not prime"
+		else:
+			i+=1
+	return"prime"
+
+print(is_prime(5191))
+ 
+
+
+
+
